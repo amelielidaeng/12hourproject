2f75b198066e964eb3647e6f0d02b61079b9bff3
amelielidaeng <98023444+amelielidaeng@users.noreply.github.com>
2022-04-18 12:42:31 +0200

    margins paddings etc + js and toggle button menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
-const toggleButton = document.getElementsByClassName('toggle')[0]
-const headerNav = document.getElementsByClassName('menu')[0]
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
-    headerNav.classList.toggle('active')
+    navbarLinks.classList.toggle('active')
 })
